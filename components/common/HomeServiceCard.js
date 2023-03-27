b29e02a430bcf87ce73acd3fc02af5df12bbb9cf
Muhammad Ali Khan <117829651+makali786@users.noreply.github.com>
2023-03-27 17:28:59 +0500

    pull request

--- components/common/HomeServiceCard.js
+++ components/common/HomeServiceCard.js
@@ -10,29 +10,30 @@ const HomeServiceCard = ({
 	return (
 		<>
 			<div className="shadow-lg p-10 w-[500px] h-[380px] ">
-				<div className="h-72"><div className="flex justify-between items-center">
-					<div className="flex flex-col">
-						<div>{item?.title}</div>
-						
-						<div className="pt-4">{item?.description}</div>
-					</div>
+				<div className="h-72">
+					<div className="flex justify-between items-center">
+						<div className="flex flex-col">
+							<div>{item?.title}</div>
 
-					<div className="border py-2 px-1.5 rounded-lg bg-primary">
-						<img src={item?.icon}></img>
+							<div className="pt-4">{item?.description}</div>
+						</div>
+
+						<div className="border py-2 px-1.5 rounded-lg bg-primary">
+							<img src={item?.icon}></img>
+						</div>
+					</div>
+					<div className="pt-4 ">
+						{item?.services.map((service, index) => {
+							return <div key={index}>{service}</div>;
+						})}
 					</div>
 				</div>
-				<div className="pt-4 ">
-					{item?.services.map((service, index) => {
-						return <div key={index}>{service}</div>;
-					})}
-				</div>
-                </div>
 				<div className="flex justify-center items-center">
 					<Button
 						customClass={
 							"border border-primary text-primary !w-[100px] !h-[30px] rounded-lg"
 						}
-						text={"Try Now"}
+						text={"Try Now q"}
 					/>
 				</div>
 			</div>
