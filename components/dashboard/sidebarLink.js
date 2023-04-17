e0a5da1401d8874b5cbec434f5dbf1fba1c8f85f
hasham140 <hashamtanveer140@gmail.com>
2023-04-17 21:58:08 +0500

    set issues

--- components/dashboard/sidebarLink.js
+++ components/dashboard/sidebarLink.js
@@ -2,20 +2,22 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 const SidebarLink = ({ sideLink, sideText, sideIcon }) => {
 	const router = useRouter();
+	console.log(router.asPath);
+	console.log(sideLink, "link");
 	return (
 		<div className="">
 			<Link href={sideLink}>
 				<div className={`text-icon  w-full `}>
 					<div
 						className={`side-bar-icon-sel ${
-							router.asPath.includes(sideLink) && "!text-primary"
+							router.asPath === sideLink && "!text-primary"
 						} `}
 					>
 						{sideIcon}
 					</div>
 					<p
 						className={`icon-para-sel ${
-							router.asPath.includes(sideLink) && "!text-primary !font-bold"
+							router.asPath === sideLink && "!text-primary !font-bold"
 						} `}
 					>
 						{sideText}
