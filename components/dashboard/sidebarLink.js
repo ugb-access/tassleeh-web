2e40d3c511c748417beb62a01212df30517b7876
hasham140 <hashamtanveer140@gmail.com>
2023-04-19 11:54:30 +0500

    set condition on hover side bar

--- components/dashboard/sidebarLink.js
+++ components/dashboard/sidebarLink.js
@@ -10,14 +10,14 @@ const SidebarLink = ({ sideLink, sideText, sideIcon }) => {
 				<div className={`text-icon  w-full `}>
 					<div
 						className={`side-bar-icon-sel ${
-							router.asPath === sideLink && "!text-primary"
+							router.asPath.includes(sideLink) && "!text-primary"
 						} `}
 					>
 						{sideIcon}
 					</div>
 					<p
 						className={`icon-para-sel ${
-							router.asPath === sideLink && "!text-primary !font-bold"
+							router.asPath.includes(sideLink) && "!text-primary !font-bold"
 						} `}
 					>
 						{sideText}
