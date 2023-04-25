9f54d3975a4728fe71a3bb0dcefa32f0ac4fabeb
Muhammad Ali Khan <117829651+makali786@users.noreply.github.com>
2023-04-25 16:54:36 +0500

    messages button padding set

--- pages/provider-dashboard/messages/index.js
+++ pages/provider-dashboard/messages/index.js
@@ -574,13 +574,13 @@ const Chat = ({
 												}
 											/>
 										</div>
-										<div className="bg-[#EFF2F7] p-3 rounded-full w-full">
+										<div className="bg-[#EFF2F7] p-4 rounded-full w-full">
 											<img src="/images/searchmessico.png" alt="" />
 										</div>
-										<div className="bg-[#EFF2F7] p-5 rounded-full w-full">
+										<div className="bg-[#EFF2F7] p-4 rounded-full w-full">
 											<img src="/images/messhead (2).png" alt="" />
 										</div>
-										<div className="bg-[#EFF2F7] p-5 py-6  w-full rounded-full ">
+										<div className="bg-[#EFF2F7] p-4  w-full rounded-full ">
 											<img src="/images/messhead (1).png" alt="" />
 										</div>
 									</div>
